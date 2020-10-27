0a7a8f929cf530678ac779e940abb3be37512fde
CotherArt <cother@protonmail.com>
2020-10-27 10:56:26 -0700

    Added background image and the branch decreases its size and color

--- branch.py
+++ branch.py
@@ -6,50 +6,69 @@ from random import randint
 
 class Branch:
 
-    def __init__(self, posx=200, posy=200, color=(255,255,255)):
-        self.posx = posx
-        self.posy = posy
-        self.tam = 1
-        self.color = color
-        self.branchlist = []
-    
-    # Add pixels to the branchlist
-    def grow(self):
-        self.posy -= self.tam
-        if randint(0,1) == 0:
-            self.posx += self.tam
-        else:
-            self.posx -= self.tam
+	def __init__(self, posx=200, posy=200, color=(255,255,255)):
+		self.posx = posx
+		self.posy = posy
+		self.tam = 13 
+		self.tamint = self.tam
+		self.ofset = 2
+		self.color = color
+		self.branchlist = []
 
-        self.branchlist.append(pygame.Rect(self.posx, self.posy, self.tam, self.tam))
-    
-    # Draws the branchlist on the screen
-    def draw_branch(self, screen):
-        for i in self.branchlist:
-            pygame.draw.rect(screen, self.color, i)
-        self.grow()
-    
-    def set_tam(self, tam):
-        self.tam = tam
+	# Add pixels to the branchlist
+	def grow(self):
+		if self.tamint == 0:
+			return
 
+		self.color_change()
+		self.tam = self.tam - .2
+		self.tamint = int(self.tam)
+		self.posy -= self.tamint 
+
+		rando = randint(0, 3)
+		if rando == 0:
+			self.posx += self.ofset
+		elif rando == 3:
+			self.posx -= self.ofset
+		self.branchlist.append((pygame.Rect(self.posx, self.posy, self.tamint, self.tamint), self.color))
+		
+	# Increse the green value in the color
+	def color_change(self):
+		r = self.color[0]
+		g = self.color[1]
+		b = self.color[2]
+		g += 5
+		if g > 200:
+			return
+		self.color = (r,g,b)		
+
+	# Draws the branchlist on the screen
+	def draw_branch(self, screen):
+		for i in self.branchlist:
+			pygame.draw.rect(screen, i[1], i[0])
+		self.grow()
+	
+	def set_tam(self, tam):
+		self.tam = tam
+	def get_tam(self):
+		return int(self.tam)
+	
 if __name__ == '__main__':
-    pygame.init()
+	pygame.init()
 
-    # Display properties
-    screen = pygame.display.set_mode((400, 400))
+	# Display properties
+	screen = pygame.display.set_mode((400, 400))
 
-    bra = Branch()
+	bra = Branch()
 
-    # Main loop
-    while True:
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+	# Main loop
+	while True:
+		events = pygame.event.get()
+		for event in events:
+			if event.type == pygame.QUIT:
+				pygame.quit()
 
-        # Draw on screen
-        screen.fill((0,0,0))
-        bra.draw_branch(screen)
+		# Draw on screen
+		bra.draw_branch(screen)
 
-        pygame.display.update()
+		pygame.display.update()
--- main.py
+++ main.py
@@ -13,15 +13,14 @@ pygame.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (47,79,79)
-GREEN = (0, 128, 0)
+GREEN = (0, 100, 0)
 MAROON = (128,0,0)
 BLUE = (30,144,255)
 LIGTH_BLUE =(186, 212, 239)
 
-color_background = LIGTH_BLUE
-color_branch = BLACK
-color_ground = GRAY
-# /Colors---------------
+color_background = WHITE 
+color_branch = GREEN 
+color_ground = GREEN 
 
 # Constants
 WIDTH, HEIGHT = 480, 480
@@ -32,34 +31,38 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('pixel garden by: CotherArt')
 clock = pygame.time.Clock()
 
+# Background image
+bg = pygame.image.load("back.png")
+
 # Branch and Ground propertyes
 pixel_tam = 1
 ground_thick = 20
 seed_posx = ORIGEN_X
-seed_posy = HEIGHT - (ground_thick + pixel_tam)
+seed_posy = HEIGHT - (ground_thick)
 
 # Objeto branch
 br = Branch(seed_posx, seed_posy, color_branch)
 
 # Dibuja el suelo
 def draw_ground():
-    pygame.draw.rect(screen, color_ground, pygame.Rect(0, HEIGHT-ground_thick, WIDTH, ground_thick))
+	pygame.draw.rect(screen, color_ground, pygame.Rect(0, HEIGHT-ground_thick, WIDTH, ground_thick))
 
 
 # Main loop
 while True:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+	events = pygame.event.get()
+	for event in events:
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
 
 
-    # Draw on screen
-    screen.fill(color_background)
-    draw_ground()
-    br.draw_branch(screen)
-    
+	# Draw on screen
+	screen.blit(bg,(0,0))	
+	draw_ground()
+	br.draw_branch(screen)
+	
 
-    pygame.display.update()
-    clock.tick(30)
+	pygame.display.update()
+	sleep(0.1)
+	clock.tick(30)
