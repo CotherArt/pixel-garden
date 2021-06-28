a0c58c34bb9cc8e02d9a9cd723d02df063846036
CotherArt <cother@protonmail.com>
2021-06-28 14:16:25 -0600

    secundary branches added

--- branch.py
+++ branch.py
@@ -5,33 +5,67 @@ import pygame
 from random import randint
 
 class Branch:
+	UP, LEFT, RIGHT = 0, 1, 2
 
-	def __init__(self, posx=200, posy=200, color=(255,255,255)):
+	def __init__(self, posx=200, posy=200, color=(255,255,255), tam=13):
+		self.grow_direction = self.UP
+
 		self.posx = posx
 		self.posy = posy
-		self.tam = 13 
-		self.tamint = self.tam
+		self.tam = tam # Grosor de la rama
+		self.tamint = self.tam # Grosor de la rama sin decimales
 		self.ofset = 2
 		self.color = color
 		self.branchlist = []
+		
+		self.randomend = False
+		self.maxlen = 15
+
 
 	# Add pixels to the branchlist
 	def grow(self):
+		if self.randomend:
+			self.maxlen = randint(5, self.maxlen)
+			if len(self.branchlist) > self.maxlen:
+				return
+
 		if self.tamint == 0:
 			return
 
 		self.color_change()
 		self.tam = self.tam - .2
 		self.tamint = int(self.tam)
-		self.posy -= self.tamint 
-
 		rando = randint(0, 3)
-		if rando == 0:
-			self.posx += self.ofset
-		elif rando == 3:
-			self.posx -= self.ofset
+
+		# Set direction of growing
+		if self.grow_direction == self.UP:
+			self.posy -= self.tamint 
+			if rando == 0:
+				self.posx += self.ofset
+			elif rando == 3:
+				self.posx -= self.ofset
+
+		if self.grow_direction == self.RIGHT:
+			self.posx += self.tamint 
+			if rando == 0:
+				self.posy += self.ofset
+			elif rando == 3:
+				self.posy -= self.ofset
+
+		if self.grow_direction == self.LEFT:
+			self.posx -= self.tamint 
+			if rando == 0:
+				self.posy += self.ofset
+			elif rando == 3:
+				self.posy -= self.ofset
+
+
 		self.branchlist.append((pygame.Rect(self.posx, self.posy, self.tamint, self.tamint), self.color))
-		
+	
+	# Returns true when the branch ends
+	def is_branch_ends():
+		return len(self.branchlist) == self.maxlen
+
 	# Increse the green value in the color
 	def color_change(self):
 		r = self.color[0]
@@ -52,7 +86,17 @@ class Branch:
 		self.tam = tam
 	def get_tam(self):
 		return int(self.tam)
-	
+	def set_maxlen(self, maxlen):
+		self.maxlen = maxlen
+
+	def get_len(self):
+		return len(self.branchlist)
+	def get_rect(self, listpos):
+		return self.branchlist[listpos][0]
+	def set_direction(self, direction):
+		self.grow_direction = direction	
+
+			
 if __name__ == '__main__':
 	pygame.init()
 
--- main.py
+++ main.py
@@ -41,28 +41,46 @@ seed_posx = ORIGEN_X
 seed_posy = HEIGHT - (ground_thick)
 
 # Objeto branch
-br = Branch(seed_posx, seed_posy, color_branch)
+tree = []
+main_br = Branch(seed_posx, seed_posy, color_branch)
+tree.append(main_br)
 
 # Dibuja el suelo
 def draw_ground():
 	pygame.draw.rect(screen, color_ground, pygame.Rect(0, HEIGHT-ground_thick, WIDTH, ground_thick))
 
 
-# Main loop
-while True:
-	events = pygame.event.get()
-	for event in events:
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+def main():
+	# Main loop
+	while True:
+		events = pygame.event.get()
+		for event in events:
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
 
-	# Draw on screen
-	screen.blit(bg,(0,0))	
-	draw_ground()
-	br.draw_branch(screen)
-	
+		# Draw on screen
+		screen.blit(bg,(0,0))	
+		draw_ground()
+		if main_br.get_len() in [5, 15, 25, 35, 45]:
+			length = main_br.get_len()
+			branch = Branch(main_br.get_rect(length-1).x, main_br.get_rect(length-1).y, color=GREEN, tam=8)
+			branch.set_direction(Branch.RIGHT)
+			tree.append(branch)
 
-	pygame.display.update()
-	sleep(0.1)
-	clock.tick(30)
+		if main_br.get_len() in [10, 20, 30, 40, 50]:
+			length = main_br.get_len()
+			branch = Branch(main_br.get_rect(length-1).x, main_br.get_rect(length-1).y, color=GREEN, tam=8)
+			branch.set_direction(Branch.LEFT)
+			tree.append(branch)
+
+		for br in tree:
+			br.draw_branch(screen)
+
+		pygame.display.update()
+		sleep(0.1)
+		clock.tick(30)
+
+if __name__ == '__main__':
+	main()
